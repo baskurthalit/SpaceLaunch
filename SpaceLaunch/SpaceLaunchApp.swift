49b5c48e519f02723b4b4598026755490031e905
Halit Seyfullah Baskurt <baskurthalit@gmail.com>
2023-10-14 14:11:49 +0300

    Initial Commit

---
+++ SpaceLaunch/SpaceLaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceLaunchApp.swift
+//  SpaceLaunch
+//
+//  Created by Halit Baskurt on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SpaceLaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
